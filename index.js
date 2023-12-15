9ffbcf6ca5ca5b7b0e61b61fead753bbbf0765ab
Allan <allan.kariuki@student.moringaschool.com>
2023-12-15 21:43:31 +0300

    Completed.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting
+  if (feet <= 400) {
+    greeting = 'This one is on me!';
+  } else if (feet > 2000 && feet < 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    greeting = 'No can do.';
+  }
+  return greeting
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ let greeting
+ (city === 'NYC') ? (greeting = 'Ok, sounds good.') : (greeting = 'No go.');
+ return greeting
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let charm;
+  switch (tip) {
+    case 'generous':
+      charm = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      charm = 'Thank you.';
+      break;
+    default:
+      charm = 'Bye.';
+      break;
+  }
+  return charm
 }
